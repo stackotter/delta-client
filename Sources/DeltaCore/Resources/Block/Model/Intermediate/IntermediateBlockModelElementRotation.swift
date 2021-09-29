93bd11767c6ab6e2cf37b901f42322ccb617e1b5
stackotter <stackotter@stackotter.dev>
2021-09-29 20:36:12 +1000

    Fix block model element y axis rotation direction

--- Sources/DeltaCore/Resources/Block/Model/Intermediate/IntermediateBlockModelElementRotation.swift
+++ Sources/DeltaCore/Resources/Block/Model/Intermediate/IntermediateBlockModelElementRotation.swift
@@ -23,8 +23,6 @@ public struct IntermediateBlockModelElementRotation {
     degrees = degrees - (degrees.truncatingRemainder(dividingBy: 22.5))
     radians = MathUtil.radians(from: Float(degrees))
     
-    // TODO: fix element rotation direction
-    
     // For some reason in our renderer we need x and y rotation to be reversed but only
     // for rotations from mojang block model files everything else is fine?
     switch axis {
@@ -42,7 +40,6 @@ public struct IntermediateBlockModelElementRotation {
     matrix *= MatrixUtil.rotationMatrix(radians, around: axis)
     if rescale {
       let scale = 1/cos(radians)
-      print("scale: \(scale), angle: \(MathUtil.degrees(from: radians))")
       matrix *= MatrixUtil.scalingMatrix(
         axis == .x ? 1 : scale,
         axis == .y ? 1 : scale,
