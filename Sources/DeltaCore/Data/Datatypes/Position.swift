93046a2ee6699125d23bb9e97a471730e3430122
stackotter <stackotter@stackotter.dev>
2021-10-01 17:21:49 +1000

    Clean up Position API

--- Sources/DeltaCore/Data/Datatypes/Position.swift
+++ Sources/DeltaCore/Data/Datatypes/Position.swift
@@ -1,11 +1,20 @@
 import Foundation
 import simd
 
+/// A block position.
 public struct Position {
   public var x: Int
   public var y: Int
   public var z: Int
   
+  /// Create a new block position.
+  ///
+  /// Coordinates are not validated.
+  ///
+  /// - Parameters:
+  ///   - x: The x coordinate.
+  ///   - y: The y coordinate.
+  ///   - z: The z coordinate.
   public init(x: Int, y: Int, z: Int) {
     self.x = x
     self.y = y
@@ -67,15 +76,23 @@ public struct Position {
     return Position(x: lhs.x &+ Int(rhs.x), y: lhs.y &+ Int(rhs.y), z: lhs.z &+ Int(rhs.z))
   }
   
-  /**
-   The block index of the position
-   
-   Blocks are placed in order of increasing x-coordinate, in rows of increasing
-   z-coordinate, in layers of increasing y. If that doesn't make sense read the
-   implementation.
-   */
+  /// The block index of the position.
+  ///
+  /// Block indices are in order of increasing x-coordinate, in rows of increasing
+  /// z-coordinate, in layers of increasing y. If that doesn't make sense read the
+  /// implementation.
   public var blockIndex: Int {
     return (y &* Chunk.depth &+ z) &* Chunk.width &+ x
+  }
+  
+  /// The biomes index of the position.
+  ///
+  /// Biome indices are in order of increasing x-coordinate, in rows of increasing
+  /// z-coordinate, in layers of increasing y. If that doesn't make sense read the
+  /// implementation. Each biome is a 4x4x4 area, and that's the only reason that
+  /// this calculation differs from `blockIndex`.
+  public var biomeIndex: Int {
+    return (y / 4 &* Chunk.depth / 4 &+ z / 4) &* Chunk.width / 4 &+ x / 4
   }
   
   /// The section Y of the section this position is in
