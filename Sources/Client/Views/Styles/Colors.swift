6c50ea3df593eac63f1f71a1f93148f7dc5e05dc
AlbertoSamele <51241132+AlbertoSamele@users.noreply.github.com>
2021-11-06 00:31:35 +0100

    Add troubleshooting menu #24
    Code refactor: styling
    
    Co-authored-by: stackotter <stackotter@stackotter.dev>

--- Sources/Client/Views/Styles/Colors.swift
+++ Sources/Client/Views/Styles/Colors.swift
@@ -2,14 +2,7 @@ import Foundation
 import SwiftUI
 
 enum Colors {
-  case primaryDarkGray, secondaryDarkGray
-  case warningYellow
-  
-  var color: Color {
-    switch self {
-    case .primaryDarkGray: return Color(red: 32/255, green: 32/255, blue: 32/255)
-    case .secondaryDarkGray: return Color(red: 83/255, green: 83/255, blue: 83/255)
-    case .warningYellow: return Color(red: 231/255, green: 241/255, blue: 89/255)
-    }
-  }
+  static var primaryDarkGray = Color(red: 32/255, green: 32/255, blue: 32/255)
+  static var secondaryDarkGray = Color(red: 83/255, green: 83/255, blue: 83/255)
+  static var warningYellow = Color(red: 231/255, green: 241/255, blue: 89/255)
 }
