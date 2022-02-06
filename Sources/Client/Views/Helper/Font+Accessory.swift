c57553c084ec5944618bcc96acbd8cbb34ff5ba9
Alberto Samele <>
2022-02-06 22:07:13 +0100

    Loading screen - new launcher #82

---
+++ Sources/Client/Views/Helper/Font+Accessory.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+
+extension Font {
+  
+  /// Fonts provided by the application- Raw value is the font string identifier
+  public enum CustomFontType: String {
+    case minecraft = "minecraft"
+    case worksans = "WorkSans-Regular"
+  }
+  
+  /// - Parameters:
+  ///   - type: the font preset
+  ///   - size: the font size
+  /// - Returns: the custom font
+  public static func custom(_ type: CustomFontType, size: CGFloat) -> Font {
+    return Font.custom(type.rawValue, size: size)
+  }
+  
+}
