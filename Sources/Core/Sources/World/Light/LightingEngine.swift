99a9c80e7b7d1c15ab2f22fe644614a9fdf6ab2b
stackotter <stackotter@stackotter.dev>
2022-02-06 16:19:30 +1000

    Refactor LightingEngine to fix linter warnings

--- Sources/Core/Sources/World/Light/LightingEngine.swift
+++ Sources/Core/Sources/World/Light/LightingEngine.swift
@@ -1,23 +1,53 @@
 import Foundation
 import Collections
 
-// The algorithm used in this lighting engine is a simplified version of the one found in
-// Starlight, a highly optimised replacement for the vanilla lighting system.
-// https://github.com/PaperMC/Starlight/tree/fabric
-
-// TODO: Handle conditionally transparent blocks (i.e. slabs and stairs)
-
-/// The engine used to update lighting of worlds when blocks change. Implementation is based of Starlight.
+/// The engine used to update a world's lighting data when a block changes.
+///
+/// The algorithm used in this lighting engine is a simplified version of the one found in
+/// [Starlight](https://github.com/PaperMC/Starlight/tree/fabric), a highly optimised replacement for the vanilla lighting system.
 public struct LightingEngine {
-  /// Queue of blocks to check for increasing light levels. Contains position of block, light level of block,
-  /// extra instructions to perform before the usual increase check, and directions to skip checking in.
-  private var increaseQueue: Deque<(Position, Int, IncreaseFlags, [Direction])> = []
-  /// Queue of blocks to check for decreasing light levels. Contains position of block, light level of block,
-  /// and directions to skip checking in.
-  private var decreaseQueue: Deque<(Position, Int, [Direction])> = []
+  // MARK: Private types
+  
+  /// An entry in the increase queue.
+  private struct IncreaseQueueEntry {
+    var position: Position
+    var lightLevel: Int
+    var flags: IncreaseFlags
+    var skipDirections: [Direction]
+  }
+  
+  /// An entry in the decrease queue.
+  private struct DecreaseQueueEntry {
+    var position: Position
+    var lightLevel: Int
+    var skipDirections: [Direction]
+  }
+  
+  /// Flags for entries in the increase queue.
+  private struct IncreaseFlags: OptionSet {
+    var rawValue: UInt8
+    
+    init(rawValue: UInt8) {
+      self.rawValue = rawValue
+    }
+    
+    static let writeLevel = IncreaseFlags(rawValue: 1)
+    static let recheckLevels = IncreaseFlags(rawValue: 2)
+  }
+  
+  // MARK: Private properties
+  
+  /// Queue of blocks to check when increasing light levels.
+  private var increaseQueue: Deque<IncreaseQueueEntry> = []
+  /// Queue of blocks to check when decreasing light levels.
+  private var decreaseQueue: Deque<DecreaseQueueEntry> = []
+  
+  // MARK: Init
   
   /// Create a new lighting engine instance with its own queues.
   public init() { }
+  
+  // MARK: Public methods
   
   /// Propagates lighting changes from an updated block. `position` is in world coordinates.
   public mutating func updateLighting(at position: Position, in world: World) {
@@ -31,6 +61,8 @@ public struct LightingEngine {
     updateBlockLight(at: positions, in: world)
     updateSkyLight(at: positions, in: world)
   }
+  
+  // MARK: Private methods
   
   /// Propagates block light changes from multiple updated blocks. `positions` are in world coordinates.
   private mutating func updateBlockLight(at positions: [Position], in world: World) {
@@ -62,7 +94,7 @@ public struct LightingEngine {
           position.y -= 1
           if world.getBlock(at: position).lightMaterial.opacity == 0 && world.getSkyLightLevel(at: position) != 15 {
             world.setSkyLightLevel(at: position, to: 15)
-            increaseQueue.append((position, 15, [], [.up, .down]))
+            increaseQueue.append(IncreaseQueueEntry(position: position, lightLevel: 15, flags: [], skipDirections: [.up, .down]))
           } else {
             break
           }
@@ -75,7 +107,7 @@ public struct LightingEngine {
           let currentLightLevel = world.getSkyLightLevel(at: position)
           if world.getBlock(at: position).lightMaterial.opacity == 0 && currentLightLevel == 15 {
             world.setSkyLightLevel(at: position, to: 0)
-            decreaseQueue.append((position, currentLightLevel, []))
+            decreaseQueue.append(DecreaseQueueEntry(position: position, lightLevel: currentLightLevel, skipDirections: []))
           }
         }
       }
@@ -98,10 +130,10 @@ public struct LightingEngine {
     world.setBlockLightLevel(at: position, to: emittedLight)
     
     if emittedLight != 0 {
-      increaseQueue.append((position, emittedLight, [], []))
+      increaseQueue.append(IncreaseQueueEntry(position: position, lightLevel: emittedLight, flags: [], skipDirections: []))
     }
     
-    decreaseQueue.append((position, currentLightLevel, []))
+    decreaseQueue.append(DecreaseQueueEntry(position: position, lightLevel: currentLightLevel, skipDirections: []))
   }
   
   /// Checks an updated block and queues any sky light level propagation required.
@@ -109,12 +141,12 @@ public struct LightingEngine {
     let currentLightLevel = world.getSkyLightLevel(at: position)
     
     if currentLightLevel == 15 {
-      increaseQueue.append((position, 15, [], []))
+      increaseQueue.append(IncreaseQueueEntry(position: position, lightLevel: 15, flags: [], skipDirections: []))
     } else {
       world.setSkyLightLevel(at: position, to: 0)
     }
     
-    decreaseQueue.append((position, currentLightLevel, []))
+    decreaseQueue.append(DecreaseQueueEntry(position: position, lightLevel: currentLightLevel, skipDirections: []))
   }
   
   private mutating func performLightLevelIncrease(
@@ -122,19 +154,17 @@ public struct LightingEngine {
     getLightLevel: (_ position: Position) -> Int,
     setLightLevel: (_ position: Position, _ level: Int) -> Void
   ) {
-    while let (position, light, flags, skipDirections) = increaseQueue.popFirst() {
-      if flags.contains(.writeLevel) {
-        setLightLevel(position, light)
+    while let entry = increaseQueue.popFirst() {
+      if entry.flags.contains(.writeLevel) {
+        setLightLevel(entry.position, entry.lightLevel)
       }
       
-      if flags.contains(.recheckLevels) {
-        if getLightLevel(position) != light {
-          continue
-        }
+      if entry.flags.contains(.recheckLevels) && getLightLevel(entry.position) != entry.lightLevel {
+        continue
       }
       
-      for direction in Direction.allDirections where !skipDirections.contains(direction) {
-        let neighbourPosition = position + direction.intVector
+      for direction in Direction.allDirections where !entry.skipDirections.contains(direction) {
+        let neighbourPosition = entry.position + direction.intVector
         
         if !world.isPositionLoaded(neighbourPosition) {
           continue
@@ -142,19 +172,19 @@ public struct LightingEngine {
         
         let neighbourLight = getLightLevel(neighbourPosition)
         
-        if neighbourLight >= light {
+        if neighbourLight >= entry.lightLevel {
           continue
         }
         
         let neighbourBlock = world.getBlock(at: neighbourPosition)
         let opacity = max(neighbourBlock.lightMaterial.opacity, 1)
-        let newNeighbourLight = max(light - opacity, 0)
+        let newNeighbourLight = max(entry.lightLevel - opacity, 0)
         
         if newNeighbourLight > neighbourLight {
           setLightLevel(neighbourPosition, newNeighbourLight)
           
           if newNeighbourLight > 1 {
-            increaseQueue.append((neighbourPosition, newNeighbourLight, [], [direction.opposite]))
+            increaseQueue.append(IncreaseQueueEntry(position: neighbourPosition, lightLevel: newNeighbourLight, flags: [], skipDirections: [direction.opposite]))
           }
         }
       }
@@ -166,9 +196,9 @@ public struct LightingEngine {
     getLightLevel: (_ position: Position) -> Int,
     setLightLevel: (_ position: Position, _ level: Int) -> Void
   ) {
-    while let (position, propagatedLight, skipDirections) = decreaseQueue.popFirst() {
-      for direction in Direction.allDirections where !skipDirections.contains(direction) {
-        let neighbourPosition = position + direction.intVector
+    while let entry = decreaseQueue.popFirst() {
+      for direction in Direction.allDirections where !entry.skipDirections.contains(direction) {
+        let neighbourPosition = entry.position + direction.intVector
         
         if !world.isPositionLoaded(neighbourPosition) {
           continue
@@ -182,40 +212,26 @@ public struct LightingEngine {
         
         let neighbourBlock = world.getBlock(at: neighbourPosition)
         let opacity = max(neighbourBlock.lightMaterial.opacity, 1)
-        let newNeighbourLight = max(propagatedLight - opacity, 0)
+        let newNeighbourLight = max(entry.lightLevel - opacity, 0)
         
         if neighbourLight > newNeighbourLight {
-          increaseQueue.append((neighbourPosition, neighbourLight, [.recheckLevels], []))
+          increaseQueue.append(IncreaseQueueEntry(position: neighbourPosition, lightLevel: neighbourLight, flags: [.recheckLevels], skipDirections: []))
           continue
         }
         
         let neighbourLuminance = neighbourBlock.lightMaterial.luminance
         if neighbourLuminance != 0 {
-          increaseQueue.append((neighbourPosition, neighbourLuminance, [.writeLevel], []))
+          increaseQueue.append(IncreaseQueueEntry(position: neighbourPosition, lightLevel: neighbourLuminance, flags: [.writeLevel], skipDirections: []))
         }
         
         setLightLevel(neighbourPosition, neighbourLuminance)
         
         if newNeighbourLight > 0 {
-          decreaseQueue.append((neighbourPosition, newNeighbourLight, [direction.opposite]))
+          decreaseQueue.append(DecreaseQueueEntry(position: neighbourPosition, lightLevel: newNeighbourLight, skipDirections: [direction.opposite]))
         }
       }
     }
     
     performLightLevelIncrease(in: world, getLightLevel: getLightLevel, setLightLevel: setLightLevel)
-  }
-}
-
-extension LightingEngine {
-  /// Flags for entries in the increase queue.
-  private struct IncreaseFlags: OptionSet {
-    var rawValue: UInt8
-    
-    init(rawValue: UInt8) {
-      self.rawValue = rawValue
-    }
-    
-    static let writeLevel = IncreaseFlags(rawValue: 1)
-    static let recheckLevels = IncreaseFlags(rawValue: 2)
   }
 }
