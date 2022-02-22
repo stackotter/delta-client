ceaa5720ffbf10edb1ade63263e4adcdf47364ad
stackotter <stackotter@stackotter.dev>
2022-02-23 08:13:34 +1000

    Smoothly interpolate mouse input

--- Sources/Core/Sources/ECS/Component/EntityRotation.swift
+++ Sources/Core/Sources/ECS/Component/EntityRotation.swift
@@ -1,18 +1,77 @@
 import FirebladeECS
+import Foundation
 
 /// A component storing an entity's rotation in radians.
 public class EntityRotation: Component {
+  /// The amount of time taken (in seconds) for ``smoothVector`` to transition from one position to the next.
+  public var smoothingAmount: Float
+  
   /// Pitch in radians.
-  public var pitch: Float
+  public var pitch: Float {
+    get {
+      _pitch
+    }
+    set(newValue) {
+      previousPitch = _pitch
+      _pitch = newValue
+      pitchLastUpdated = CFAbsoluteTimeGetCurrent()
+    }
+  }
+  
   /// Yaw in radians.
-  public var yaw: Float
+  public var yaw: Float {
+    get {
+      _yaw
+    }
+    set(newValue) {
+      previousYaw = _yaw
+      _yaw = newValue
+      yawLastUpdated = CFAbsoluteTimeGetCurrent()
+    }
+  }
+  
+  /// The smoothly interpolated pitch.
+  public var smoothPitch: Float {
+    let delta = Float(CFAbsoluteTimeGetCurrent() - pitchLastUpdated)
+    let progress = MathUtil.clamp(delta / smoothingAmount, 0, 1)
+    return MathUtil.lerpAngle(from: previousPitch, to: _pitch, progress: progress)
+  }
+  
+  /// The smoothly interpolated yaw.
+  public var smoothYaw: Float {
+    let delta = Float(CFAbsoluteTimeGetCurrent() - yawLastUpdated)
+    let progress = MathUtil.clamp(delta / smoothingAmount, 0, 1)
+    return MathUtil.lerpAngle(from: previousYaw, to: _yaw, progress: progress)
+  }
+  
+  // MARK: Private properties
+  
+  /// The current pitch.
+  private var _pitch: Float
+  /// The previous pitch.
+  private var previousPitch: Float
+  /// The time pitch was last updated. Used for smoothing.
+  private var pitchLastUpdated: CFAbsoluteTime
+  
+  /// The current yaw.
+  private var _yaw: Float
+  /// The previous yaw.
+  private var previousYaw: Float
+  /// The time yaw was last updated. Used for smoothing.
+  private var yawLastUpdated: CFAbsoluteTime
   
   /// Creates an entity's rotation.
   /// - Parameters:
   ///   - pitch: The pitch in radians. -pi/2 is straight up, 0 is straight ahead, and pi/2 is straight down.
   ///   - yaw: The yaw in radians. Measured counterclockwise from the positive z axis.
-  public init(pitch: Float, yaw: Float) {
-    self.pitch = pitch
-    self.yaw = yaw
+  ///   - smoothingAmount: The amount of time (in seconds) for ``smoothYaw`` and ``smoothPitch`` to transition from one position to the next. Defaults to 0 seconds.
+  public init(pitch: Float, yaw: Float, smoothingAmount: Float = 0) {
+    self._pitch = pitch
+    self._yaw = yaw
+    self.previousPitch = pitch
+    self.previousYaw = yaw
+    self.pitchLastUpdated = CFAbsoluteTimeGetCurrent()
+    self.yawLastUpdated = CFAbsoluteTimeGetCurrent()
+    self.smoothingAmount = smoothingAmount
   }
 }
--- Sources/Core/Sources/Util/MathUtil.swift
+++ Sources/Core/Sources/Util/MathUtil.swift
@@ -27,6 +27,32 @@ public enum MathUtil {
     return 180 * radians / .pi
   }
   
+  /// Linearly interpolates between two floating point values.
+  /// - Parameters:
+  ///   - initial: The initial value.
+  ///   - target: The target value.
+  ///   - progress: The progress from the initial value to the target value (gets clamped between 0 and 1).
+  /// - Returns: The current value, linearly interpolated between the initial and target values.
+  public static func lerp<T: FloatingPoint>(from initial: T, to target: T, progress: T) -> T {
+    let progress = MathUtil.clamp(progress, 0, 1)
+    return initial + progress * (target - initial)
+  }
+  
+  /// Linearly interpolates between two angles (in radians).
+  /// - Parameters:
+  ///   - initial: The initial angle in radians.
+  ///   - target: The target angle in radians.
+  ///   - progress: The progress from the initial angle to the target angle (gets clamped between 0 and 1).
+  /// - Returns: The current angle, linearly interpolated between the initial and target angles.
+  public static func lerpAngle<T: FloatingPoint>(from initial: T, to target: T, progress: T) -> T {
+    let max: T = .pi * 2;
+    // Difference is the smallest angle between the initial and target angles (for example, lerping from 359 degrees to 0, the difference would be 1)
+    var difference = (target - initial).remainder(dividingBy: max);
+    difference = (2 * difference).remainder(dividingBy: max) - difference;
+    let progress = MathUtil.clamp(progress, 0, 1)
+    return initial + progress * difference
+  }
+  
   /// Calculates `a` (mod `n`). `n` must be positive.
   ///
   /// Swift's `%` isn't actually mod, it is remainder and doesn't behave the
@@ -46,19 +72,33 @@ public enum MathUtil {
     return SIMD3<Float>(doubleArray.map { Float($0) })
   }
   
-  // TODO: use swift numerics package instead
+  /// Checks for approximate equality between two floats.
+  /// - Parameters:
+  ///   - a: The first float.
+  ///   - b: The second float.
+  ///   - absoluteTolerance: The tolerance for what is acceptable equality.
+  /// - Returns: The approximate equality.
   public static func checkFloatEquality(_ a: Float, _ b: Float, absoluteTolerance: Float) -> Bool {
+    // TODO: use swift numerics package instead
     return abs(a - b) <= absoluteTolerance
   }
   
   public static func checkFloatLessThan(value a: Float, compareTo b: Float, absoluteTolerance: Float) -> Bool {
+    // TODO: use swift numerics package instead
     return a - b < absoluteTolerance
   }
   
   public static func checkFloatGreaterThan(value a: Float, compareTo b: Float, absoluteTolerance: Float) -> Bool {
+    // TODO: use swift numerics package instead
     return b - a < absoluteTolerance
   }
   
+  /// Clamps a given value between a maximum and minimum value.
+  /// - Parameters:
+  ///   - value: The value to clamp.
+  ///   - minValue: The minimum value.
+  ///   - maxValue: The maximum value.
+  /// - Returns: The clamped value.
   @_specialize(where T:_Trivial)
   public static func clamp<T>(_ value: T, _ minValue: T, _ maxValue: T) -> T where T : Comparable {
     return min(max(value, minValue), maxValue)
