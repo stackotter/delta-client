9b77e79ff9039dd43344b570b6af71030c9c4f1d
stackotter <stackotter@stackotter.dev>
2022-04-15 07:47:22 +1000

    Fix jumping physics to match vanilla

--- Sources/Core/Sources/ECS/Systems/VelocitySystem.swift
+++ Sources/Core/Sources/ECS/Systems/VelocitySystem.swift
@@ -7,8 +7,8 @@ public struct VelocitySystem: System {
     // Apply velocity to all moving entities (excluding the player).
     let physicsEntities = nexus.family(requiresAll: EntityPosition.self, EntityVelocity.self, excludesAll: ClientPlayerEntity.self)
     for (position, velocity) in physicsEntities {
-//      position.save()
-//      position.move(by: velocity.vector)
+      position.save()
+      position.move(by: velocity.vector)
     }
   }
 }
