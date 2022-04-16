2396636fde35fa16c8ad9587b67d6348fb76fcfc
stackotter <stackotter@stackotter.dev>
2022-04-16 12:36:24 +1000

    Fix walk-jumping physics to match vanilla perfectly

--- Sources/Core/Sources/ECS/Components/EntityOnGround.swift
+++ Sources/Core/Sources/ECS/Components/EntityOnGround.swift
@@ -2,9 +2,18 @@ import FirebladeECS
 
 /// A component storing whether an entity is on the ground (walking or swimming) or not on the ground (jumping/falling).
 public class EntityOnGround: Component {
+  /// Whether the entity is touching the ground or not.
   public var onGround: Bool
+  /// The most recently saved value of ``onGround`` (see ``save()``).
+  public var previousOnGround: Bool
   
   public init(_ onGround: Bool) {
     self.onGround = onGround
+    previousOnGround = onGround
+  }
+
+  /// Saves the current value to ``previousOnGround``.
+  public func save() {
+    previousOnGround = onGround
   }
 }
