b7d3e87006195fd93f12be7ca76713a96dd2ea02
stackotter <stackotter@stackotter.dev>
2022-04-16 12:36:24 +1000

    Fix walk-jumping physics to match vanilla perfectly

--- Sources/Core/Sources/ECS/Systems/PlayerGravitySystem.swift
+++ Sources/Core/Sources/ECS/Systems/PlayerGravitySystem.swift
@@ -1,14 +1,16 @@
 import FirebladeECS
+import Foundation
 
 public struct PlayerGravitySystem: System {
   public func update(_ nexus: Nexus, _ world: World) {
     var family = nexus.family(
       requiresAll: EntityFlying.self,
+      EntityPosition.self,
       EntityVelocity.self,
       ClientPlayerEntity.self
     ).makeIterator()
     
-    guard let (flying, velocity, _) = family.next() else {
+    guard let (flying, position, velocity, _) = family.next() else {
       log.error("PlayerGravitySystem failed to get player to tick")
       return
     }
@@ -19,5 +21,8 @@ public struct PlayerGravitySystem: System {
     
     velocity.y -= 0.08
     velocity.y *= 0.98
+
+    print("position: (\(position.x), \(position.y), \(position.z))")
+    print("velocity: (\(velocity.x), \(velocity.y), \(velocity.z))")
   }
 }
--- Sources/Core/Sources/ECS/Systems/PlayerSmoothingSystem.swift
+++ Sources/Core/Sources/ECS/Systems/PlayerSmoothingSystem.swift
@@ -5,15 +5,17 @@ public struct PlayerSmoothingSystem: System {
     var family = nexus.family(
       requiresAll: EntityPosition.self,
       EntityRotation.self,
+      EntityOnGround.self,
       ClientPlayerEntity.self
     ).makeIterator()
     
-    guard let (position, rotation, _) = family.next() else {
+    guard let (position, rotation, onGround, _) = family.next() else {
       log.error("PlayerSmoothingSystem failed to get player to tick")
       return
     }
     
     position.save()
     rotation.save()
+    onGround.save()
   }
 }
