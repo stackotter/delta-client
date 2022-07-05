533845c0f1a56d1392fded0660427ef7ba2bb5e8
stackotter <stackotter@stackotter.dev>
2022-07-05 11:14:11 +1000

    Limit number of concurrent event handlers run at once to 20

--- Sources/Core/Sources/Plugin/Event/EventBus.swift
+++ Sources/Core/Sources/Plugin/Event/EventBus.swift
@@ -1,33 +1,45 @@
 import Foundation
 
-/// A simple yet flexible thread-safe subscriber-based event system. Events are dispatched concurrently.
+/// A simple yet flexible thread-safe subscriber-based event system. Events are dispatched
+/// concurrently.
 public class EventBus {
+  /// The maximum number of handlers that are allowed to run concurrently.
+  public static let maximumConcurrentHandlers = 20
+
   /// The array of registered event handlers.
   private var handlers: [(Event) -> Void] = []
   /// A lock for managing thread safe read and write of ``handlers``.
   private var handlersLock = ReadWriteLock()
-  
+
   /// The concurrent dispatch queue for dispatching events.
-  private var eventThread = DispatchQueue(label: "events", attributes: [.concurrent])
+  private var dispatchQueue = DispatchQueue(label: "events", attributes: .concurrent)
+  /// The serial dispatch queue for managing event dispatches without blocking callers.
+  private var managementQueue = DispatchQueue(label: "EventBus.managementQueue")
+  /// Used to limit the number of event handlers run at once.
+  private var semaphore = DispatchSemaphore(value: maximumConcurrentHandlers)
 
   /// Creates a new event bus.
   public init() {}
-  
+
   /// Registers a handler to receive updates.
   public func registerHandler(_ handler: @escaping (Event) -> Void) {
     handlersLock.acquireWriteLock()
     defer { handlersLock.unlock() }
     handlers.append(handler)
   }
-  
-  /// Concurrently sends an event to all registered handlers.
+
+  /// Concurrently sends an event to all registered handlers. Doesn't block the calling thread.
   public func dispatch(_ event: Event) {
-    handlersLock.acquireReadLock()
-    defer { handlersLock.unlock() }
-    
-    for handler in handlers {
-      eventThread.async {
-        handler(event)
+    managementQueue.async {
+      self.handlersLock.acquireReadLock()
+      defer { self.handlersLock.unlock() }
+
+      for handler in self.handlers {
+        self.semaphore.wait()
+        self.dispatchQueue.async {
+          handler(event)
+          self.semaphore.signal()
+        }
       }
     }
   }
