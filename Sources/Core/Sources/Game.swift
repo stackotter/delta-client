b6aa04b97c53e80320d116175364446c9a8a7a3b
Gracien <gjwsky@icloud.com>
2022-10-08 15:00:37 +0200

    Toggle block outline rendering based on gamemode (#138)
    * Gamemode dependent block targetability. No outline in Spectator mode.
    
    * Gamemode check performed in the worldRenderer
    
    * Fix guard initialisation. Add comment.
    
    * Remove computed variable. Direct check in rendering loop.

--- Sources/Core/Sources/Game.swift
+++ Sources/Core/Sources/Game.swift
@@ -267,6 +267,7 @@ public struct Game {
   /// Gets the position of the block currently targeted by the player.
   public func targetedBlock() -> BlockPosition? {
     var ray: Ray = Ray(origin: .zero, direction: .zero)
+    
     accessPlayer { player in
       ray = player.ray
     }
@@ -285,6 +286,16 @@ public struct Game {
     }
 
     return nil
+  }
+  
+  /// Gets current gamemode of the player
+  public func currentGamemode() -> Gamemode? {
+    var gamemode: Gamemode? = nil
+    accessPlayer { player in
+      gamemode = player.gamemode.gamemode
+    }
+    
+    return gamemode
   }
 
   // MARK: Lifecycle
