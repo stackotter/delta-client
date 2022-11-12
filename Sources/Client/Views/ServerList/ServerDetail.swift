a01b1c7485337af8b9b9502d4c8f14ee8f2b4496
stackotter <stackotter@stackotter.dev>
2022-11-13 09:25:49 +1000

    Display human-readable error message when pinging fails

--- Sources/Client/Views/ServerList/ServerDetail.swift
+++ Sources/Client/Views/ServerList/ServerDetail.swift
@@ -4,7 +4,7 @@ import DeltaCore
 struct ServerDetail: View {
   @EnvironmentObject var appState: StateWrapper<AppState>
   @ObservedObject var pinger: Pinger
-  
+
   var body: some View {
     let descriptor = pinger.descriptor
     VStack(alignment: .leading) {
@@ -12,7 +12,7 @@ struct ServerDetail: View {
         .font(.title)
       Text(descriptor.description)
         .padding(.bottom, 8)
-      
+
       if let result = pinger.response {
         switch result {
           case let .success(response):
@@ -21,22 +21,22 @@ struct ServerDetail: View {
               fontSize: FontUtil.systemFontSize(for: .regular),
               alignment: .right
             ).padding(.bottom, 8)
-            
+
             Text(verbatim: "\(response.players.online)/\(response.players.max) online")
-            
+
             LegacyFormattedTextView(
               legacyString: "Version: \(response.version.name) \(response.version.protocolVersion == Constants.protocolVersion ? "" : "(incompatible)")",
               fontSize: FontUtil.systemFontSize(for: .regular),
               alignment: .center
             ).padding(.bottom, 8)
-            
+
             Button("Play") {
               appState.update(to: .playServer(descriptor))
             }
               .buttonStyle(PrimaryButtonStyle())
               .frame(width: 150)
           case let .failure(error):
-            Text(String("Connection failed: \(error)"))
+            Text(error.localizedDescription)
               .padding(.bottom, 8)
             Button("Play") { }
               .buttonStyle(DisabledButtonStyle())
