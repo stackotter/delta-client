587fa2367d8275329fc0523529f3d39bcc6843f0
stackotter <stackotter@stackotter.dev>
2022-11-14 20:44:40 +1000

    Remove linter plugin because of dependency conflicts

--- Package.swift
+++ Package.swift
@@ -2,20 +2,6 @@
 
 import PackageDescription
 
-var dependencies: [Package.Dependency] = [
-  // See Notes/PluginSystem.md for more details on the architecture of the project in regards to dependencies, targets and linking
-  // In short, the dependencies for DeltaCore can be found in Sources/Core/Package.swift
-  .package(name: "DeltaCore", path: "Sources/Core"),
-  .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
-  .package(url: "https://github.com/stackotter/SwordRPC", .revision("3ddf125eeb3d83cb17a6e4cda685f9c80e0d4bed")),
-  .package(url: "https://github.com/stackotter/swift-cross-ui", branch: "main")
-]
-
-#if swift(>=5.6)
-// Add linter if swift version is high enough
-dependencies.append(.package(url: "https://github.com/stackotter/swift-lint-plugin", from: "0.1.0"))
-#endif
-
 var products: [Product] = [
   .executable(
     name: "DeltaClientGtk",
@@ -87,6 +73,13 @@ let package = Package(
   name: "DeltaClient",
   platforms: [.macOS(.v11), .iOS(.v15)],
   products: products,
-  dependencies: dependencies,
+  dependencies: [
+    // See Notes/PluginSystem.md for more details on the architecture of the project in regards to dependencies, targets and linking
+    // In short, the dependencies for DeltaCore can be found in Sources/Core/Package.swift
+    .package(name: "DeltaCore", path: "Sources/Core"),
+    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
+    .package(url: "https://github.com/stackotter/SwordRPC", .revision("3ddf125eeb3d83cb17a6e4cda685f9c80e0d4bed")),
+    .package(url: "https://github.com/stackotter/swift-cross-ui", branch: "main")
+  ],
   targets: targets
 )
--- Sources/Core/Package.swift
+++ Sources/Core/Package.swift
@@ -2,31 +2,6 @@
 
 import PackageDescription
 
-// MARK: Dependencies
-
-var dependencies: [Package.Dependency] = [
-  .package(name: "ZIPFoundation", url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.0"),
-  .package(name: "DeltaLogger", url: "https://github.com/stackotter/delta-logger", .branch("main")),
-  .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
-  .package(name: "swift-collections", url: "https://github.com/apple/swift-collections.git", from: "0.0.7"),
-  .package(name: "swift-atomics", url: "https://github.com/apple/swift-atomics.git", from: "1.0.2"),
-  .package(name: "FirebladeECS", url: "https://github.com/stackotter/ecs.git", .branch("master")),
-  .package(name: "ZippyJSON", url: "https://github.com/michaeleisel/ZippyJSON", from: "1.2.4"),
-  .package(url: "https://github.com/pointfreeco/swift-parsing", .exact("0.8.0")),
-  .package(url: "https://github.com/stackotter/fireblade-math.git", branch: "matrix2x2"),
-  .package(url: "https://github.com/seznam/swift-resolver", from: "0.3.0"),
-  .package(url: "https://github.com/fourplusone/swift-package-zlib", from: "1.2.11"),
-  .package(url: "https://github.com/stackotter/swift-image.git", branch: "master"),
-  .package(url: "https://github.com/OpenCombine/OpenCombine.git", from: "0.13.0"),
-  .package(url: "https://github.com/kelvin13/swift-png", from: "4.0.2"),
-  .package(url: "https://github.com/stackotter/ASN1Parser", branch: "main"),
-  .package(url: "https://github.com/krzyzanowskim/CryptoSwift", from: "1.6.0")
-]
-
-#if swift(>=5.6)
-dependencies.append(.package(url: "https://github.com/stackotter/swift-lint-plugin", from: "0.1.0"))
-#endif
-
 // MARK: Products
 
 var productTargets = ["DeltaCore"]
@@ -94,6 +69,23 @@ let package = Package(
     .library(name: "DeltaCore", type: .dynamic, targets: productTargets),
     .library(name: "StaticDeltaCore", type: .static, targets: productTargets)
   ],
-  dependencies: dependencies,
+  dependencies: [
+    .package(name: "ZIPFoundation", url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.0"),
+    .package(name: "DeltaLogger", url: "https://github.com/stackotter/delta-logger", .branch("main")),
+    .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
+    .package(name: "swift-collections", url: "https://github.com/apple/swift-collections.git", from: "0.0.7"),
+    .package(name: "swift-atomics", url: "https://github.com/apple/swift-atomics.git", from: "1.0.2"),
+    .package(name: "FirebladeECS", url: "https://github.com/stackotter/ecs.git", .branch("master")),
+    .package(name: "ZippyJSON", url: "https://github.com/michaeleisel/ZippyJSON", from: "1.2.4"),
+    .package(url: "https://github.com/pointfreeco/swift-parsing", .exact("0.8.0")),
+    .package(url: "https://github.com/stackotter/fireblade-math.git", branch: "matrix2x2"),
+    .package(url: "https://github.com/seznam/swift-resolver", from: "0.3.0"),
+    .package(url: "https://github.com/fourplusone/swift-package-zlib", from: "1.2.11"),
+    .package(url: "https://github.com/stackotter/swift-image.git", branch: "master"),
+    .package(url: "https://github.com/OpenCombine/OpenCombine.git", from: "0.13.0"),
+    .package(url: "https://github.com/kelvin13/swift-png", from: "4.0.2"),
+    .package(url: "https://github.com/stackotter/ASN1Parser", branch: "main"),
+    .package(url: "https://github.com/krzyzanowskim/CryptoSwift", from: "1.6.0")
+  ],
   targets: targets
 )
