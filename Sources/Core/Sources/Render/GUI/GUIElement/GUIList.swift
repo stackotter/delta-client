1e185ccfa48b13e769d88c124e96c741d5227476
JWhitmore1 <38652089+JWhitmore1@users.noreply.github.com>
2023-01-09 23:52:32 +1000

    Implemented right alignment for text in debug menu

--- Sources/Core/Sources/Render/GUI/GUIElement/GUIList.swift
+++ Sources/Core/Sources/Render/GUI/GUIElement/GUIList.swift
@@ -2,11 +2,13 @@ struct GUIList: GUIElement {
   var items: [GUIListItem]
   var rowHeight: Int
   var renderRowBackground: Bool
+  var rightAlign: Bool
 
-  init(rowHeight: Int, renderRowBackground: Bool = false) {
+  init(rowHeight: Int, renderRowBackground: Bool = false, rightAlign: Bool = false) {
     items = []
     self.rowHeight = rowHeight
     self.renderRowBackground = renderRowBackground
+    self.rightAlign = rightAlign
   }
 
   mutating func add(_ element: GUIElement) {
@@ -20,21 +22,47 @@ struct GUIList: GUIElement {
   func meshes(context: GUIContext) throws -> [GUIElementMesh] {
     var bgMeshes: [GUIElementMesh] = []
     var meshes: [GUIElementMesh] = []
+    var processedItems: [(GUIListItem, [GUIElementMesh])] = []
+    var maxWidth = 0
     var currentY = 0
 
+    // block to find longest row of elements 
     for item in items {
       switch item {
         case .element(let element):
-          var elementMeshes = try element.meshes(context: context)
-          elementMeshes.translate(amount: [0, currentY])
+          let elementMeshes = try element.meshes(context: context)
+          processedItems.append((item, elementMeshes))
+          if elementMeshes.size().x > maxWidth {
+            maxWidth = elementMeshes.size().x
+          }
+        case .spacer:
+          processedItems.append((item, []))
+      }
+    }
+
+    for (item, var elementMeshes) in processedItems {
+      switch item {
+        case .element:
+          switch rightAlign {
+            case false:
+              elementMeshes.translate(amount: [0, currentY])
+            case true:
+              elementMeshes.translate(amount: [maxWidth - elementMeshes.size().x, currentY])
+          }
 
           if renderRowBackground {
-            let bgSize: SIMD2<Int> = [elementMeshes.size().x + 1, rowHeight]
+            let bgSize: SIMD2<Int> = [elementMeshes.size().x + 2, rowHeight]
             var bg = GUIRectangle(
               size: bgSize,
               color: [0x50, 0x50, 0x50, 0x90] / 255
             ).meshes(context: context)
-            bg.translate(amount: [-1, currentY - 1])
+            switch rightAlign {
+              case false:
+                bg.translate(amount: [-1, currentY - 1])
+              case true:
+                bg.translate(amount: [maxWidth - elementMeshes.size().x - 1, currentY - 1])
+            }
+            
             bgMeshes.append(contentsOf: bg)
           }
 
