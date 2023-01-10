128fc2e95554a35613aa052c6f81aa289bf7c310
stackotter <stackotter@stackotter.dev>
2023-01-10 23:57:27 +1000

    Update GitHub Action to Xcode 14.2

--- Sources/Core/Sources/Network/Protocol/Packets/Play/Serverbound/PlayerDiggingPacket.swift
+++ Sources/Core/Sources/Network/Protocol/Packets/Play/Serverbound/PlayerDiggingPacket.swift
@@ -2,11 +2,11 @@ import Foundation
 
 public struct PlayerDiggingPacket: ServerboundPacket {
   public static let id: Int = 0x1b
-  
+
   public var status: DiggingStatus
   public var location: BlockPosition
   public var face: Direction
-  
+
   public enum DiggingStatus: Int32 {
     case startedDigging = 0
     case cancelledDigging = 1
@@ -16,7 +16,7 @@ public struct PlayerDiggingPacket: ServerboundPacket {
     case shootArrowOrFinishEating = 5
     case swapItemInHand = 6
   }
-  
+
   public func writePayload(to writer: inout PacketWriter) {
     writer.writeVarInt(status.rawValue)
     writer.writePosition(location)
