d1246b619161172e64c3bccf75d7c2ba89c34456
jxhug <me@jxhug.me>
2023-08-05 19:57:33 -0700

    working

--- Sources/Core/Sources/ECS/Singles/InputState.swift
+++ Sources/Core/Sources/ECS/Singles/InputState.swift
@@ -24,6 +24,14 @@ public final class InputState: SingleComponent {
   /// The position of the right thumbstick.
   public private(set) var rightThumbstick: Vec2f = Vec2f(0, 0)
 
+  /// The time since the last time the player pressed the forwards key.
+  public private(set) var forwardsDownTime: Int = 0
+  /// The time since the last time the player released the forwards key.
+  public private(set) var forwardsUpTime: Int = 0
+  /// Counts the ticks
+  public private(set) var tickCount: Int = 0
+
+
   // MARK: Init
 
   /// Creates an empty input state.
@@ -86,10 +94,21 @@ public final class InputState: SingleComponent {
   /// Ticks the input state by flushing ``newlyPressed`` into ``keys`` and ``inputs``, and clearing
   /// ``newlyReleased``. Also emits events to the given ``EventBus``.
   func tick(_ isInputSuppressed: [Bool], _ eventBus: EventBus) {
+    tickCount += 1
+    /// increment the time since the forwards key was pressed if it is currently pressed
     assert(isInputSuppressed.count == newlyPressed.count, "`isInputSuppressed` should be the same length as `newlyPressed`")
     for (var event, suppressInput) in zip(newlyPressed, isInputSuppressed) {
       if suppressInput {
         event.input = nil
+      }
+      ///test for forwards key
+      if event.input == .moveForward {
+        if forwardsDownTime < forwardsUpTime && (forwardsDownTime + 6) > tickCount {
+          inputs.insert(.sprint)
+        } else {
+          inputs.remove(.sprint)
+        }
+        forwardsDownTime = tickCount
       }
 
       eventBus.dispatch(event)
@@ -103,6 +122,10 @@ public final class InputState: SingleComponent {
     }
 
     for event in newlyReleased {
+      if event.input == .moveForward {
+        forwardsUpTime = tickCount
+      }
+      
       // TODO: The release event of any inputs that were suppressed should probably also be suppressed
       eventBus.dispatch(event)
 
