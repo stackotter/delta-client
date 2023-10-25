09f0904a8feca61b34c254f843db90e924a869a9
ninjadev64 <63245705+ninjadev64@users.noreply.github.com>
2023-10-25 17:10:43 +0100

    Implement editing and removing servers in DeltaClientGtk

--- Sources/ClientGtk/ServerListView.swift
+++ Sources/ClientGtk/ServerListView.swift
@@ -1,15 +1,20 @@
 import SwiftCrossUI
 import DeltaCore
 
-enum DetailState {
-  case addition
-  case server(ServerDescriptor)
-  case error(String)
+indirect enum DetailState {
+  case server(_ index: Int)
+  case adding
+  case editing(_ index: Int)
+
+  case error(_ message: String, returnState: DetailState?)
+  static func error(_ message: String) -> DetailState {
+    return .error(message, returnState: nil)
+  }
 }
 
 class ServerListViewState: Observable {
   @Observed var servers: [ServerDescriptor] = []
-  @Observed var detailState = DetailState.addition
+  @Observed var detailState = DetailState.adding
 
   @Observed var name = ""
   @Observed var address = ""
@@ -24,13 +29,13 @@ struct ServerListView: View {
     NavigationSplitView {
       VStack {
         Button("Add server") {
-          state.detailState = .addition
+          state.detailState = .adding
         }
 
         ScrollView {
-          ForEach(state.servers) { server in
-            Button(server.name) {
-              state.detailState = .server(server)
+          ForEach(state.servers.indices) { index in
+            Button(state.servers[index].name) {
+              state.detailState = .server(index)
             }
             .padding(.bottom, 5)
           }
@@ -41,23 +46,18 @@ struct ServerListView: View {
     } detail: {
       VStack {
         switch state.detailState {
-          case .addition:
-            additionView
-          case .server(let server):
-            Text(server.name)
-            if let port = server.port {
-              Text("\(server.host):\(port)")
-            } else {
-              Text(server.host)
-            }
-
-            Button("Connect") {
-              completionHandler(server)
-            }
-          case .error(let message):
+          case .server(let index):
+            serverView(index)
+          case .adding:
+            addingView()
+          case .editing(let index):
+            editingView(index)
+          case .error(let message, let returnState):
             Text("Error: \(message)")
-            Button ("Back") {
-              state.detailState = .addition
+            if let returnState {
+              Button ("Back") {
+                state.detailState = returnState
+              }
             }
         }
       }
@@ -65,37 +65,116 @@ struct ServerListView: View {
     }
   }
 
-  var additionView: some View {
-    VStack {
+  func serverView(_ index: Int) -> some View {
+    let server = state.servers[index]
+
+    return VStack {
+      Text(server.name)
+      if let port = server.port {
+        Text("\(server.host):\(port)")
+      } else {
+        Text(server.host)
+      }
+
+      Button("Connect") {
+        completionHandler(server)
+      }
+
+      Button("Edit") {
+        state.name = server.name
+        if let port = server.port {
+          state.address = "\(server.host):\(port)"
+        } else {
+          state.address = "\(server.host)"
+        }
+        state.detailState = .editing(index)
+      }
+    }
+  }
+
+  func addingView() -> some View {
+    return VStack {
       Text("Add server")
       TextField("Name", state.$name)
       TextField("Address", state.$address)
       
       Button("Add") {
-        let parts = state.address.split(separator: ":")
-        guard parts.count > 0, parts.count <= 2 else {
-          state.detailState = .error("Invalid address")
-          return
+        do {
+          let (host, port) = try ServerListView.parseAddress(state.address)
+
+          let descriptor = ServerDescriptor(name: state.name, host: host, port: port)
+          state.servers.append(descriptor)
+          state.detailState = .server(state.servers.count - 1)
+          
+          state.name = ""
+          state.address = ""
+        } catch AddressParsingError.invalidPort {
+          state.detailState = .error("Invalid port", returnState: .adding)
+        } catch {
+          state.detailState = .error("Invalid address", returnState: .adding)
         }
-
-        let host = String(parts[0])
-
-        var port: UInt16?
-        if parts.count == 2 {
-          guard let parsed = UInt16(parts[1]) else {
-            state.detailState = .error("Invalid port")
-            return
-          }
-          port = parsed
-        }
-
-        let descriptor = ServerDescriptor(name: state.name, host: host, port: port)
-        state.servers.append(descriptor)
-        state.detailState = .server(descriptor)
-        
-        state.name = ""
-        state.address = ""
       }
     }
+  }
+
+  func editingView(_ index: Int) -> some View {
+    return VStack {
+      Text("Edit server")
+      TextField("Name", state.$name)
+      TextField("Address", state.$address)
+
+      Button("Apply") {
+        do {
+          let (host, port) = try ServerListView.parseAddress(state.address)
+
+          state.servers[index].name = state.name
+          state.servers[index].host = host
+          state.servers[index].port = port
+          state.detailState = .server(index)
+
+          state.name = ""
+          state.address = ""
+        } catch AddressParsingError.invalidPort {
+          state.detailState = .error("Invalid port", returnState: .editing(index))
+        } catch {
+          state.detailState = .error("Invalid address", returnState: .editing(index))
+        }
+      }
+
+      Button("Remove") {
+        state.servers.remove(at: index)
+        state.name = ""
+        state.address = ""
+        if (state.servers.count > 0) {
+          state.detailState = .server(0)
+        } else {
+          state.detailState = .adding
+        }
+      }
+    }
+  }
+
+  private enum AddressParsingError: Error {
+    case invalidColonAmount
+    case invalidPort
+  }
+
+  private static func parseAddress(_ address: String) throws -> (host: String, port: UInt16?) {
+    let parts = address.split(separator: ":")
+    guard parts.count > 0, parts.count <= 2 else {
+      throw AddressParsingError.invalidColonAmount
+    }
+
+    let host = String(parts[0])
+
+    var port: UInt16?
+    if parts.count == 2 {
+      guard let parsed = UInt16(parts[1]) else {
+        throw AddressParsingError.invalidPort
+      }
+      port = parsed
+    }
+
+    return (host, port)
   }
 }
